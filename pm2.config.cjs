085777fa377bd50023fbbdd67d141ed04381571b
bugugugu <1759743261@qq.com>
2025-09-03 15:38:21 +0800

    新增pm2.config.cjs 配置文件

---
+++ pm2.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "bugu-code-bits",
+      script: "/app/code/bugu-code-bits/.output/server/index.mjs",
+      env: {
+        PORT: 8080
+      }
+    }
+  ]
+}
