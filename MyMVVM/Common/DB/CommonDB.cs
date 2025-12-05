44bb364f6e4900c376d9686261701a0b4412e848
bitm <1759743261@qq.com>
2025-12-05 11:49:20 +0800

    控制是否弹出摄像仪

--- MyMVVM/Common/DB/CommonDB.cs
+++ MyMVVM/Common/DB/CommonDB.cs
@@ -522,7 +522,7 @@ namespace MyMVVM.Common
 
         public static bool QueryNumberIsHasMonitor(string number)
         {
-            string sql = $"select count(*) as count from dm_user where usernum = '{number}' and camera_ip IS NOT NULL;";
+            string sql = $"select count(*) as count from dm_user where usernum = '{number}' and camera_ip IS NOT NULL and camera_status != '0';";
             DataTable dispatch_number_table = DB.ExecuteQuery(sql);
             return dispatch_number_table.Rows[0]["count"].ToString() != "0";
         }
